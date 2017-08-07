1e2b24a8ea039842790ae43c12dc6ce205e42013
NamgyuCho <southq82@gmail.com>
2017-08-07 09:32:16 +0900

    DCGAN model

--- DCGAN/model.py
+++ DCGAN/model.py
@@ -87,4 +87,153 @@ class DCGAN(object):
     def build_mode(self):
         if self.y_dim:
             self.y_dim = tf.placeholder(tf.float32, [self.batch_size, self.y_dim], name='y')
+        else:
+            self.y = None
+
+        if self.crop:
+            image_dims = [self.output_height, self.output_width, self.c_dim]
+        else:
+            image_dims = [self.input_height, self.input_width, self.c_dim]
+
+        self.inputs = tf.placeholder(tf.float32, [self.batch_size] + image_dims, name='real_images')
+
+        inputs = self.inputs
+
+        self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
+        self.z_sum = histogram_summary('z', self.z)
+
+        self.G = self.generator(self.z, self.y)
+        self.D, self.D_logits = self.discriminator(inputs, self.y, reuse=False)
+        self.sampler = self.sampler(self.z, self.y)
+        self.D_, self.D_logits = self.discriminator(self.G, self.y, reuse=True)
+
+        self.d_sum = histogram_summary('d', self.D)
+        self.d__sum = histogram_summary('d_', self.D_)
+        self.G_sum = image_summary('G', self.G)
+
+
+        def sigmoid_cross_entory_with_logits(x, y):
+            try:
+                return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, labels=y)
+            except:
+                return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, targets=y)
+
+        self.d_loss_real = tf.reduce_mean(
+            sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
+        self.d_loss_fake = tf.reduce_mean(
+            sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
+        self.g_loss = tf.reduce_mean(
+            sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_)))
+
+        self.d_loss_real_sum = scalar_summary('d_loss_real', self.d_loss_real)
+        self.d_loss_face_sum = scalar_summary('d_loss_fake', self.d_loss_fake)
+
+        self.d_loss = self.d_loss_real + self.d_loss_fake
+
+        self.g_loss_sum = scalar_summary('g_loss', self.g_loss)
+        self.d_loss_sum = scalar_summary('d_loss', self.d_loss)
+
+        t_vars = tf.trainabble_variables()
+
+        self.d_vars = [var for var in t_vars if 'd_' in var.name]
+        self.g_vars = [var for var in t_vars if 'g_' in var.name]
+
+        self.saver = tf.train.Saver()
+
+    def train(self, config):
+        d_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1)\
+            .minimize(self.d_loss, var_list=self.d_vars)
+        g_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1)\
+            .minimize(self.g_loss, var_list=self.g_vars)
+
+        try:
+            tf.global_variable_initializer().run()
+        except:
+            tf.initialize_all_variabbles().run()
+
+        self.g_sum = merge_summary([self.z_sum, self.d__sum, self.G_sum, self.d_loss_face_sum, self.g_loss_sum])
+        self.d_sum = merge_summary([self.z_sum, self.d_sum, self.d_loss_real_sum, self.d_loss_sum])
+        self.writer = SummaryWriter('./logs', self.sess.graph)
+
+        sample_z = np.random_uniform(-1, 1, size=(self.sample_num, self.z_dim))
+
+        if config.dataset == 'mnist':
+            sample_inputs = self.data_X[0:self.sample_num]
+            sample_labels = self.data_y[0:self.sample_num]
+        else:
+            sample_files = self.data[0:self.sample_num]
+            sample = [
+                get_image(sample_file,
+                          input_height=self.input_height,
+                          input_width=self.input_width,
+                          resize_height=self.output_height,
+                          resize_width=self.output_width,
+                          crop=self.crop,
+                          grayscale=self.grayscale) for sample_file in sample_files
+                ]
+            if (self.grayscale):
+                sample_inputs = np.array(sample).astype(np.float32)[:, :, :, None]
+            else:
+                sample_inputs = np.array(sample).astype(np.float32)
+
+        counter = 1
+        start_time = time.time()
+        could_load, checkpoint_counter = self.load(self.checkpoint_dir)
+
+        if could_load:
+            counter = checkpoint_counter
+            print(' [*] Load SUCCESS')
+        else:
+            print(' [!] Load failed..')
+
+        for epoch in xrange(config.epoch):
+            if config.data == 'mnist':
+                batch_idxs = min(len(self.data_X), config.train_size) // config.batch_size
+            else:
+                self.data = glob(os.path.join(
+                    './data', config.dataset, set.input_fname_pattern))
+                batch_idxs = min(len(self.data), config.train_size) // config.batch_size
+
+            for idx in xrange(0, batch_idxs):
+                if config.dataset == 'mnist':
+                    batch_images = self.data_X[idx*config.batch_size:(idx+1)*config.batch_size]
+                    batch_labels = self.data_y[idx*config.batch_size:(idx+1)*config.batch_size]
+                else:
+                    batch_files = self.data[idx*config.batch_size:(idx+1)*config.batch_size]
+                    batch = [get_image(batch_file,
+                                        input_height=self.input_height,
+                                        input_width=self.input_width,
+                                        resize_height=self.output_height,
+                                        resize_width=self.output_width,
+                                        crop=self.crop,
+                                        grayscale=self.grayscale) for batch_file in batch_files]
+                    if self.grayscale:
+                        batch_images = np.array(batch).astype(np.float32)[:, :, :, None]
+                    else:
+                        batch_images = np.array(batch).astype(np.float32)
+
+                batch_z = np.random.uniform(-1, 1, [config.batch_size, self.z_dim]).astype(np.float32)
+
+                if config.dataset == 'mnist':
+                    # Update D network
+                    _, summary_str = self.sess.run([d_optim, self.d_sum],
+                                                   feed_dict={
+                                                       self.inputs: batch_images,
+                                                       self.z: batch_z,
+                                                       self.y: batch_labels,
+                                                   })
+                    self.writer.add_summary(summary_str, counter)
+
+                    # Update G network
+                    _, summary_str = self.sess.run([g_optim, self.g_sum],
+                                                   feed_dict={
+                                                       self.z: batch_z,
+                                                       self.y: batch_labels,
+                                                   })
+                    self.writer.add_summary(summary_str, counter)
+
+                    # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
+                    _, summary_str = self.sess.run([g_optim, self.g_sum],
+                                                   feed_dict={self.z: batch_z, self.y: batch_labels})
+                    self.writer.add_summary(summary_str, counter)
 
